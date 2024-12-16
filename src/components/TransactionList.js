783a9151f22a7c3a45631ad0d6281f9ef2091b5b
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-16 19:41:43 +0545

    GlobalContext added on TransactionList.js file and shows all the transactions text

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
   return (
     <>
         <h3>History</h3> 
-        <ul id='list' className='list'>
-            <li className='minus'>
-                Cash <span>- $400</span>
+        <ul className='list'>
+            {transactions.map(transaction => (<li className='minus'>
+                {transaction.text} <span>- $400</span>
                 <button className='delete-btn'>X</button>
-            </li>
+            </li>))}
+            
         </ul>
     </>
   )
