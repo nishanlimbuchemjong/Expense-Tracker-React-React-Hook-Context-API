8f5e68b5c338fbbb04febb086dcbe2d9fb1c35c3
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-16 19:59:51 +0545

    adding css on App.css file, also handling border color as green for income, or red for expense

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Transaction = (props) => {
     const sign = props.transaction.amount <0?'-':'+';
   return (
-    <li className='minus'>
+    <li className={props.transaction.amount < 0 ? 'minus': 'plus'}>
         {props.transaction.text} <span>{sign} ${Math.abs(props.transaction.amount)}</span>
         <button className='delete-btn'>X</button>
     </li>
