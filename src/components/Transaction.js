52d74839b1d9e4ab510fb5dc7b62a558eb4564df
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-17 18:07:13 +0545

    DeleteTransaction function added on GlobalState.js and used the action for delete on AppReducer.js file, and used on Transaction.js file.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = (props) => {
-    const sign = props.transaction.amount <0?'-':'+';
+  const {DeleteTransaction} = useContext(GlobalContext);
+
+  const sign = props.transaction.amount <0?'-':'+'; 
   return (
     <li className={props.transaction.amount < 0 ? 'minus': 'plus'}>
         {props.transaction.text} <span>{sign} ${Math.abs(props.transaction.amount)}</span>
-        <button className='delete-btn'>X</button>
+        <button onClick={()=> DeleteTransaction(props.transaction.id)}className='delete-btn'>X</button>
     </li>
   )
 }
